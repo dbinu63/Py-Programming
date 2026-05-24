da20d38384f217d66d71c2331a5c559842c24b34
Don Binu <150541671+dbinu63@users.noreply.github.com>
2026-05-24 16:08:15 -0500

    initial commit

---
+++ week1/notes/keywords.py
@@ -0,0 +1,11 @@
+import keyword as kw
+
+print(kw.kwlist, "\n\nLength:", len(kw.kwlist))
+# Comments are made with '#' or 3 quotes (single & double work)
+
+'''
+x = 1
+for kw in kw.kwlist:
+    print(f"{x}. {kw}\n")
+    x += 1
+'''
